bb590e355c66a41c2aa2cfd9483df9157345423f
Colin David Scott <colin@abstractcode.com>
2020-05-07 23:12:39 +1000

    Markdown table converter

---
+++ src/main/scala/com/abstractcode/unifimarkdownextractor/MarkdownTableConverter.scala
@@ -0,0 +1,19 @@
+package com.abstractcode.unifimarkdownextractor
+
+import cats.data.NonEmptyList
+
+object MarkdownTableConverter {
+  case class Column[T](title: String, extractor: T => String)
+
+  def convert[T](columns: NonEmptyList[Column[T]])(data: NonEmptyList[T]): String = {
+    def convertSingle(item: T): String = {
+      columns.foldLeft("|")((b, column) => s"$b ${column.extractor(item).replace("|", "\\|")} |")
+    }
+
+    val header = columns.foldLeft("|")((b, column) =>s"$b ${column.title} |")
+    val separator = columns.foldLeft("|")((b, _) =>s"$b---|")
+    val content = data.map(convertSingle).toList.mkString("\n")
+
+    s"$header\n$separator\n$content"
+  }
+}
---
+++ src/test/scala/com/abstractcode/unifimarkdownextractor/MarkdownTableConverterSpec.scala
@@ -0,0 +1,88 @@
+package com.abstractcode.unifimarkdownextractor
+
+import cats.data.NonEmptyList
+import com.abstractcode.unifimarkdownextractor.MarkdownTableConverter.Column
+import org.specs2.Specification
+import org.specs2.matcher.MatchResult
+import org.specs2.specification.core.SpecStructure
+
+class MarkdownTableConverterSpec extends Specification {
+  case class DataItem(one: String, two: Int, three: Double)
+
+  def is: SpecStructure =
+    s2"""
+         Markdown Table Converter
+            will convert single column single data $singleColumnSingleData
+            will convert multiple columns single data $multipleColumnsSingleData
+            will convert multiple columns multiple data $multipleColumnsMultipleData
+            will encode values $encodeValues
+      """
+
+  def singleColumnSingleData: MatchResult[Any] = {
+    val data = NonEmptyList.one(DataItem("test", 42, 4.5))
+
+    val columns = NonEmptyList.one(Column[DataItem]("First", _.one))
+
+    val expected =
+      """| First |
+        @|---|
+        @| test |""".stripMargin('@')
+
+    MarkdownTableConverter.convert(columns)(data) shouldEqual expected
+  }
+
+  def multipleColumnsSingleData: MatchResult[Any] = {
+    val data = NonEmptyList.one(DataItem("test", 42, 4.5))
+
+    val columns = NonEmptyList.of(
+      Column[DataItem]("First", _.one),
+      Column[DataItem]("Second", i => s"${i.two} ${i.three}"),
+    )
+
+    val expected =
+      """| First | Second |
+        @|---|---|
+        @| test | 42 4.5 |""".stripMargin('@')
+
+    MarkdownTableConverter.convert(columns)(data) shouldEqual expected
+  }
+
+  def multipleColumnsMultipleData: MatchResult[Any] = {
+    val data = NonEmptyList.of(
+      DataItem("test", 42, 4.5),
+      DataItem("also test", -5436, 100.5),
+      DataItem("something else", 333, -33)
+    )
+
+    val columns = NonEmptyList.of(
+      Column[DataItem]("First", _.one),
+      Column[DataItem]("Second", i => s"${i.two} ${i.three}"),
+    )
+
+    val expected =
+      """| First | Second |
+        @|---|---|
+        @| test | 42 4.5 |
+        @| also test | -5436 100.5 |
+        @| something else | 333 -33.0 |""".stripMargin('@')
+
+    MarkdownTableConverter.convert(columns)(data) shouldEqual expected
+  }
+  def encodeValues: MatchResult[Any] = {
+    val data = NonEmptyList.of(
+      DataItem("test | test", 42, 4.5)
+    )
+
+
+    val columns = NonEmptyList.of(
+      Column[DataItem]("First", _.one)
+    )
+
+    val expected =
+      """| First |
+        @|---|
+        @| test \| test |""".stripMargin('@')
+
+    MarkdownTableConverter.convert(columns)(data) shouldEqual expected
+  }
+}
